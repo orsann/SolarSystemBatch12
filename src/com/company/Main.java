898a76f2bdca7268f2d72ad50a0328eeed53067d
Orsan <orsann@gmai.com>
2022-05-22 21:51:29 -0700

    Planet1 features have been added successfully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        SolarSystem FactorX=new SolarSystem();
-        FactorX.Sun="1";
-        FactorX.Moon="4";
-        FactorX.Planet="2";
-        FactorX.Star="10000";
+        // write your code here
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planet = "2";
+        FactorX.Star = "10000";
 
 
 //        adding features to our sun
-        FeatureSun Xfact= new FeatureSun();
-        Xfact.color="green";
-        Xfact.radius="1000000000km";
-        Xfact.heat="309324934898kj";
+        FeatureSun Xfact = new FeatureSun();
+        Xfact.color = "green";
+        Xfact.radius = "1000000000km";
+        Xfact.heat = "309324934898kj";
 
 //        adding the features for star in the solar system
         FeatureStars FacX = new FeatureStars();
-        FacX.color="green";
-        FacX.radius="738y4932749km";
-        FacX.size="9178707005";
+        FacX.color = "green";
+        FacX.radius = "738y4932749km";
+        FacX.size = "9178707005";
+
+        //adding features for planet1
+        Planet1Features GulK = new Planet1Features();
+        GulK.color = "blue";
+        GulK.size = "123456km";
+        GulK.name="preachex";
     }
 }
